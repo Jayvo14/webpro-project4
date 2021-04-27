6dfa7f9054d3cc0d7fd8fa39cbbcbe8edad6dc91
ahmed <52543589+amdMUST@users.noreply.github.com>
2021-04-27 14:19:19 -0400

    Delete connect_to_DB.php
    - this didnt work in all cases so i removed it

--- connect_to_DB.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $servername = "localhost";
-    $username = "amustafa3";
-    $password = "amustafa3";
-    $dbname = "amustafa3";
-        
-    // Create connection
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-?>
